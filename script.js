d3cc823546c65b98a9cf04cab5328c7132fe7265
HanniWang23 <yw7669@nyu.edu>
2024-12-20 02:44:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-document.querySelectorAll('.puzzle-piece').forEach(piece => {
+document.querySelectorAll('.piece').forEach(piece => {
     piece.addEventListener('click', () => {
-        piece.classList.toggle('flipped');
-        if (!piece.querySelector('.back')) {
-            const back = document.createElement('div');
-            back.classList.add('back');
-            back.innerHTML = `
-                <strong>${piece.dataset.language}</strong>: <br>${piece.dataset.meaning}
-            `;
-            piece.appendChild(back);
-        }
+      if (!piece.style.transform || piece.style.transform === 'rotateY(0deg)') {
+        piece.style.transform = 'rotateY(180deg)';
+      } else {
+        piece.style.transform = 'rotateY(0deg)';
+      }
     });
-});
+  });
+  
