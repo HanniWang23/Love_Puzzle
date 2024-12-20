21c43b4c61bd5ba9f75b80fe93bab1e7fef5fd8c
HanniWang23 <yw7669@nyu.edu>
2024-12-20 03:49:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 document.querySelectorAll('.piece').forEach(piece => {
-  piece.addEventListener('click', () => {
-    if (!piece.style.transform || piece.style.transform === 'rotateY(0deg)') {
-      piece.style.transform = 'rotateY(180deg)';
-    } else {
-      piece.style.transform = 'rotateY(0deg)';
-    }
+    piece.addEventListener('click', () => {
+      if (!piece.style.transform || piece.style.transform === 'rotateY(0deg)') {
+        piece.style.transform = 'rotateY(180deg)';
+      } else {
+        piece.style.transform = 'rotateY(0deg)';
+      }
+    });
   });
-});
+  
